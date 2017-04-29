3808ba6f305d2c06f53783810ed5bf94d397bf56
Christian <c.guarini@studenti.unisa.it>
2017-04-30 01:35:35 +0200

    Jamm a vre

---
+++ src/providers/speakers-service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+/*
+  Generated class for the SpeakersService provider.
+
+  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
+  for more info on providers and Angular 2 DI.
+*/
+@Injectable()
+export class SpeakersService {
+
+  result: any;
+
+  constructor(public http: Http) {
+  }
+
+  load() {
+    if (this.result) {
+      return Promise.resolve(this.result);
+    }
+
+    return new Promise(resolve => {
+      this.http.get('http://193.205.163.223:8080/Symposium/resources/invitedspeakers')
+        .map(res => res.json())
+        .subscribe(data => {
+          this.result = data;
+          resolve(this.result);
+        });
+    });
+  }
+
+}
