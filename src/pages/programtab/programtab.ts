57b8578abe9bd94b6b975fe72e2699d96f7737aa
Angelo <boyangel90@hotmail.it>
2017-04-29 03:01:39 +0200

    Added DaysService provider

--- src/pages/programtab/programtab.ts
+++ src/pages/programtab/programtab.ts
@@ -1,20 +1,29 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
-import { ProgramPage } from '../program/program'
+import {Component} from '@angular/core';
+import {NavController, NavParams} from 'ionic-angular';
+import {ProgramPage} from '../program/program';
+import {DaysService} from '../../providers/days-service';
 
 @Component({
   selector: 'page-programtab',
   templateUrl: 'programtab.html',
+  providers: [DaysService]
 })
 export class ProgramTab {
 
   rootPage = ProgramPage;
+  public days: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public daysService: DaysService) {
+  }
+
+  loadDays() {
+    this.daysService.load().then(data => {
+      this.days = data;
+    });
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad ProgramTab');
+    this.loadDays();
   }
 
 }
---
+++ src/providers/days-service.ts
@@ -0,0 +1,34 @@
+import {Injectable} from '@angular/core';
+import {Http} from '@angular/http';
+import 'rxjs/add/operator/map';
+
+/*
+ Generated class for the DaysService provider.
+
+ See https://angular.io/docs/ts/latest/guide/dependency-injection.html
+ for more info on providers and Angular 2 DI.
+ */
+@Injectable()
+export class DaysService {
+
+  result: any;
+
+  constructor(public http: Http) {
+  }
+
+  load() {
+    if (this.result) {
+      return Promise.resolve(this.result);
+    }
+
+    return new Promise(resolve => {
+      this.http.get('http://localhost:8080/Symposium/resources/days')
+        .map(res => res.json())
+        .subscribe(data => {
+          this.result = data;
+          resolve(this.result);
+        });
+    });
+  }
+
+}
